17d24fd65307f613fc8cf2d8d431dcd6f20a3d2a
suryadwd <surajdwivedi644@gmail.com>
2025-07-16 11:25:54 +0530

    day3 of goal

--- terraform.tf
+++ terraform.tf
@@ -6,3 +6,8 @@ terraform {
     }
   }
 }
+
+
+# if youe .tfsate has some problem where it is corrupted/deleted then remmmmoving the .backup will make it the normal .tfstate file
+#  now my backup file is save now what if we dont have the .backaup file also then what we will do we have to check this out
+# for this scenario we will under teh terraform.tf we will add the backend folder 
