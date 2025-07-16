4615f49b90c4a27a05e8f7e1a912fe0455c6a411
suryadwd <surajdwivedi644@gmail.com>
2025-07-16 11:39:47 +0530

    day3 of goal

--- terraform.tf
+++ terraform.tf
@@ -10,4 +10,5 @@ terraform {
 
 # if youe .tfsate has some problem where it is corrupted/deleted then remmmmoving the .backup will make it the normal .tfstate file
 #  now my backup file is save now what if we dont have the .backaup file also then what we will do we have to check this out
-# for this scenario we will under teh terraform.tf we will add the backend folder 
+# for this scenario we will under teh terraform.tf we will add the backend 
+# the backend will connect to backend server then needed the bucket name thern the key (terraform.tfstate)  with region and dynamodb name
