11824de6cc0edad0dc7582d6636c785536d5116c
suryadwd <surajdwivedi644@gmail.com>
2025-07-16 13:17:20 +0530

    day3 of goals

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,10 +1,10 @@
 resource "aws_dynamodb_table" "dynamodb" {
   name           = "GameScores"
   billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "lockId"
-
+  hash_key = "LockID"
+  
   attribute {
-    name = "lockId"
+    name =  "LockID"
     type = "S"
   }
 
